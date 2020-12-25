efb12d0849fa33d177a1c155b709dd8d585e9e36
baldev <baldev064@gmail.com>
2020-12-25 15:32:54 +0530

    web project with php

---
+++ new.php
@@ -0,0 +1,20 @@
+<?php
+   echo "helloworld";
+?>
+
+<html>
+<head>
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <title>link and images</title>           
+</head>
+<body>
+   <a href="https://google.com" target="blank">go to google</a><br>
+   <a href="https://facebook.com" target="blank">go to facebook</a><br>
+   <a href="https://instagram.com" target="blank">go to instagram</a><br>
+   <a href="https://amazon.com" target="blank">go to amazon</a><br>
+   <a href="/baldev.html" target="blank"> baldev</a><br>
+   <img src="baldev.jpg" alt="image not found">
+   <img src="https://source.unsplash.com/user/erondu/800x400" alt="netural image">
+</body>
+</html>
